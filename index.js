9768641e5daa0d5fe4f9082626fe4734d4721bbc
Mohammed Noori <kmanoorib@gmail.com>
2018-03-18 00:30:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function viewCart() {
     
     stringToReturn = stringToReturn.substring(0, stringToReturn.length - 2);
     i++;
-    stringToReturn = stringToReturn + " and " +" at " + `${cart[i].itemPrice}`+".";
+    stringToReturn = stringToReturn + " and " +" at "+".";
     
     return stringToReturn;
     
